22476d62c3dbd6a32bb2a15263af5c32aa98062c
andreas.groos <andreas.groos1@gmail.com>
2018-04-09 23:37:40 -0700

    404 route handler

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import BlogPost from "./components/Blogpost.vue";
 import About from "./views/About.vue";
 import Write from "./views/Write.vue";
 import Settings from "./views/Settings.vue";
+import FourOFour from "./components/FourOFour.vue";
 
 Vue.use(Router);
 
@@ -34,6 +35,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: About
+    },
+    {
+      path: "*",
+      name: "404",
+      component: FourOFour
     }
   ]
 });
